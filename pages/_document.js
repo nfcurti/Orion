21a9d032990a34695c9f91265d77c1f6888de386
Juan Manuel <nicolascurti75@gmail.com>
2023-08-23 17:09:04 +0200

    fixes

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Footer } from '@pmndrs/branding'
 
 export default function Document() {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,59 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { motion } from "framer-motion"
-import { useRef, useState } from 'react'
+import * as THREE from 'three'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-
-function Box (props) {
-  // This reference gives us direct access to the THREE.Mesh object.
-  const ref = useRef()
-  
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-
-  // Subscribe this component to the render-loop and rotate the mesh every frame.
-  useFrame((state,delta) => (ref.current.rotation.x += delta))
-  
-  return (
-    <mesh      
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    > 
-      <boxGeometry args={[1, 1, 1]} />      
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange' } />    
-    </mesh>
-  )
-}
+import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
 
 export default function App() {
   return (
-    <Canvas>
-      <color attach="background" args={['#fff']} />
-      <ambientLight intensity={0.5} />      
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />      
-      <pointLight position={[-10, -10, -10]} />      
-      <Box position={[-1.2, 0, 0]} />     
-      <Box position={[1.2, 0, 0]} />    
+    <Canvas style={{height:"100vh"}} concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
+      <color attach="background" args={['black']} />
+      <fog attach="fog" args={['black', 15, 20]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          
+          <VideoText position={[0, 1.3, -2]} />
+          <Ground />
+        </group>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[0, 10, 0]} intensity={0.3} />
+        <directionalLight position={[-50, 0, -40]} intensity={0.7} />
+        <Intro />
+      </Suspense>
     </Canvas>
   )
+}
+
+function Carla(props) {
+  const { scene } = useGLTF('/carla-draco.glb')
+  return <primitive object={scene} {...props} />
+}
+
+function VideoText(props) {
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+  useEffect(() => void video.play(), [video])
+  return (
+    <Text font="/Inter-Bold.woff" fontSize={3} letterSpacing={-0.06} {...props}>
+      Ginevar
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+      </meshBasicMaterial>
+    </Text>
+  )
+}
+
+function Ground() {
+  const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg'])
+  return (
+    <Reflector blur={[400, 100]} resolution={512} args={[10, 10]} mirror={0.5} mixBlur={6} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+      {(Material, props) => <Material color="#a0a0a0" metalness={0.4} roughnessMap={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
+    </Reflector>
+  )
+}
+
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3())
+  return useFrame((state) => {
+    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
+    state.camera.lookAt(0, 0, 0)
+  })
 }
